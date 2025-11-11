b04d287198f566dd167cd098bff9b51d0c1f0a4d
Ian Garcez <6891905+igarcez@users.noreply.github.com>
2025-11-11 19:55:37 +0100

    simplify lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,6 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
-					"pylsp",
 					"ts_ls",
 				},
 				automatic_installation = true,
@@ -59,64 +58,14 @@ return {
 			}
 			vim.diagnostic.config(config)
 
-			-- This function gets run when an LSP connects to a particular buffer.
-            -- todo how to attach this to all clients?
-			local on_attach = function(client, bufnr)
-				local lsp_map = require("helpers.keys").lsp_map
-
-				lsp_map("<leader>lr", vim.lsp.buf.rename, bufnr, "Rename symbol")
-				lsp_map("<leader>la", vim.lsp.buf.code_action, bufnr, "Code action")
-				lsp_map("<leader>ld", vim.lsp.buf.type_definition, bufnr, "Type definition")
-				lsp_map("<leader>ls", require("telescope.builtin").lsp_document_symbols, bufnr, "Document symbols")
-
-				lsp_map("gd", vim.lsp.buf.definition, bufnr, "Goto Definition")
-				lsp_map("gr", require("telescope.builtin").lsp_references, bufnr, "Goto References")
-				lsp_map("gI", vim.lsp.buf.implementation, bufnr, "Goto Implementation")
-				lsp_map("K", vim.lsp.buf.hover, bufnr, "Hover Documentation")
-				lsp_map("gD", vim.lsp.buf.declaration, bufnr, "Goto Declaration")
-
-				-- Create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
-
-				lsp_map("<leader>ff", "<cmd>Format<cr>", bufnr, "Format")
-
-				-- Attach and configure vim-illuminate
-				require("illuminate").on_attach(client)
-			end
-
-			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-			-- Lua
             vim.lsp.config("lua_ls", {
-                on_attach = on_attach,
-                capabilities = capabilities,
                 settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" }
+                        }
+                    }
                 }
-            })
-            vim.lsp.enable({"lua_ls"})
-
-            -- TypeScript
-            vim.lsp.config("typescript-tools", {
-                on_attach = on_attach,
-                capabilities = capabilities,
             })
 		end,
 	},
