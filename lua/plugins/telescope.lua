c9778ef616dd4a1d63b8e5591bd6101b4d6a5c33
Ian Garcez <ian.garcez@sitoo.com>
2025-11-26 10:58:15 +0100

    improve telescope livegrep and add ts_ls

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,27 @@ return {
         config = function()
             require("telescope").setup({
                 defaults = {
-                    mappings = {i = {["<C-u>"] = false, ["<C-d>"] = false}}
+                    mappings = {i = {["<C-u>"] = false, ["<C-d>"] = false}},
+                    vimgrep_arguments = {
+                      "rg",
+                      "--follow",        -- Follow symbolic links
+                      "--hidden",        -- Search for hidden files
+                      "--no-heading",    -- Don't group matches by each file
+                      "--with-filename", -- Print the file path with the matched lines
+                      "--line-number",   -- Show line numbers
+                      "--column",        -- Show column numbers
+                      "--smart-case",    -- Smart case search
+
+                      -- Exclude some patterns from search
+                      "--glob=!**/.git/*",
+                      "--glob=!**/.idea/*",
+                      "--glob=!**/.vscode/*",
+                      "--glob=!**/build/*",
+                      "--glob=!**/dist/*",
+                      "--glob=!**/yarn.lock",
+                      "--glob=!**/package-lock.json",
+                      "--glob=!**/node_modules/*",
+                    },
                 }
             })
 
